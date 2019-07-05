54a5eb6b0d005b1406338fde90d73861af6111c0
Mechanik247 <247Desant>
2019-07-05 15:55:02 +0400

    fix "wrong login & password"

--- src/main/java/Servlets/AccessDeniedServlet.java
+++ src/main/java/Servlets/AccessDeniedServlet.java
@@ -8,24 +8,27 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "accessdenied", urlPatterns = "/accessdenied")
 public class AccessDeniedServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
         req.getRequestDispatcher("accessdenied.jsp").forward(req, resp);
         req.setCharacterEncoding("UTF-8");
         String login = req.getParameter("login");
         String password = req.getParameter("pass");
         ClientDAOImpl dao = new ClientDAOImpl();
         Client client = dao.login(login, password);
-        req.setAttribute("client",client);
-
-        if(client == null){
+        if (client == null) {
             req.getRequestDispatcher("/accessdenied").forward(req, resp);
         }
         else {
+            session.setAttribute("client", client);
+            session.setAttribute("name", client.getName());
+            session.setAttribute("phoneNumber", client.getPhone_number());
             req.getRequestDispatcher("/lk").forward(req, resp);
         }
     }
--- src/main/java/Servlets/LoginServlet.java
+++ src/main/java/Servlets/LoginServlet.java
@@ -16,10 +16,6 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        if(session.isNew())
-        {
-            req.getRequestDispatcher("index.html").forward(req, resp);
-        }
 
         req.setCharacterEncoding("UTF-8");
         String login = req.getParameter("login");
@@ -29,18 +25,16 @@ public class LoginServlet extends HttpServlet {
 
         if (login != null && password != null) {
             Client client = dao.login(login, password);
-            req.setAttribute("client",client);
 
-            session.setAttribute("client", client);
-            session.setAttribute("name", client.getName());
-            session.setAttribute("phoneNumber", client.getPhone_number());
-
-            System.out.println(client);
-
-            if(client == null){
+            if (client == null) {
                 req.getRequestDispatcher("/accessdenied").forward(req, resp);
             }
             else {
+                session.setAttribute("client", client);
+                session.setAttribute("name", client.getName());
+                session.setAttribute("phoneNumber", client.getPhone_number());
+
+                System.out.println(client);
                 req.getRequestDispatcher("/lk").forward(req, resp);
             }
         }
